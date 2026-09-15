2eb73ac3e22645ec128fb5c67fe9b71d5555ab35
JS0801 <jsuthar@oncloudconsulting.ca>
2026-09-15 16:34:15 -0400

    Update from NetSuite (Auto)

--- BC_SL_SO_PDF_Helper_(4).js
+++ BC_SL_SO_PDF_Helper_(4).js
@@ -78,6 +78,7 @@ define(['N/ui/serverWidget', 'N/search', 'N/log', 'N/file', 'N/encode', 'N/runti
             })
           ]
         });
+        salesorderSearchObj.save()
         
         const employeeMap = {};
         const uniqueDates = new Set();
